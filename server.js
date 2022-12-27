a8269774ce13f91ada1e79b491b27861cd1c8446
My Lhassane <mylhassanedeveloper@gmail.com>
2022-12-27 20:24:36 +0100

    setup route list of users

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
 const express = require('express')
 const app = express()
 
+const users = [
+    { id: 01, name: 'User 01' },
+    { id: 02, name: 'User 02' },
+    { id: 03, name: 'User 03' },
+    { id: 04, name: 'User 04' },
+    { id: 05, name: 'User 05' },
+    { id: 06, name: 'User 06' },
+    { id: 07, name: 'User 07' },
+    { id: 08, name: 'User 08' },
+    { id: 09, name: 'User 09' },
+    { id: 10, name: 'User 10' },
+    { id: 11, name: 'User 11' },
+    { id: 12, name: 'User 12' },
+    { id: 13, name: 'User 13' },
+]
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
 app.listen(3000)
