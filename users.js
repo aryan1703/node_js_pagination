90cf9195c1757e51891abcefc9d04fbbd1d0b12c
My Lhassane <mylhassanedeveloper@gmail.com>
2022-12-31 09:40:22 +0100

    Setup MongoDB: user schema

---
+++ users.js
@@ -0,0 +1,10 @@
+const mongoose = 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
