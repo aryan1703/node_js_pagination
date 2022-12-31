88a15276773ef542482f5ca73477ed6cca55e54c
My Lhassane <mylhassanedeveloper@gmail.com>
2022-12-31 10:21:05 +0100

    Populate some default data inside of our model

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const User = require('./users')
 
 mongoose.connect('mongodb://localhost/pagination', {useNewUrlParser: true, useUnifiedTopology: true})
 
+const db = mongoose.connection
+db.once('open', async () => {
+    if (await User.countDocuments().exec() > 0) return
+
+    Promise.all([
+        User.create({ name: 'User 01' }),
+        User.create({ name: 'User 02' }),
+        User.create({ name: 'User 03' }),
+        User.create({ name: 'User 04' }),
+        User.create({ name: 'User 05' }),
+        User.create({ name: 'User 06' }),
+        User.create({ name: 'User 07' }),
+        User.create({ name: 'User 08' }),
+        User.create({ name: 'User 09' }),
+        User.create({ name: 'User 10' }),
+    ]).then(() => console.log('Added Users'))
+})
+
 app.get('/users', paginatedResults(User), (req, res) => {
     res.json(res.paginatedResults)
 })
